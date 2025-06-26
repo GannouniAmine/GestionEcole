870a5d6f9609155eeebf19ff76af78f14ad0fbd6
GannouniAmine <Mohamedamine.gannouni@ensi-uma.tn>
2025-06-26 13:22:48 +0100

    ✨ Ajout de la méthode pour modifier un professeur

--- etude/src/main/java/com/app/etude/etude/controleur/ProfesseurControleur.java
+++ etude/src/main/java/com/app/etude/etude/controleur/ProfesseurControleur.java
@@ -2,29 +2,40 @@ package com.app.etude.etude.controleur;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.etude.etude.dto.Professeurdto;
 import com.app.etude.etude.models.Professeur;
 import com.app.etude.etude.services.ProfesseurServices;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping("/api/Professeur")
+@RequestMapping("/api/v1/Professeur")
+@RequiredArgsConstructor
 public class ProfesseurControleur {
 	
-	private ProfesseurServices Professeurservices;
+	private final ProfesseurServices Professeurservices;
 	
 	@GetMapping("/allProfesseur")
-    public List<Professeur> findAllProfesseurs()
+    public List<Professeurdto> findAllProfesseurs()
 	{
-		return Professeurservices.findAllProfesseurs();
+		return Professeurservices.findAll();
 	}
 	@GetMapping("/{id}")
-    public Professeur findProfesseurbyid(@PathVariable Long id)
+    public Professeurdto findProfesseurbyid(@PathVariable Long id)
 	{
-		return Professeurservices.findProfesseurbyid(id);
+		return Professeurservices.findProfById(id);
+	}
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Void> deleteClasses(@PathVariable("id") Long id){
+		Professeurservices.deleteProfById(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- etude/src/main/java/com/app/etude/etude/services/ProfesseurServices.java
+++ etude/src/main/java/com/app/etude/etude/services/ProfesseurServices.java
@@ -1,12 +1,33 @@
 package com.app.etude.etude.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
+import com.app.etude.etude.dto.Professeurdto;
 import com.app.etude.etude.models.Professeur;
+import com.app.etude.etude.repository.ProfesseurRepository;
 
-public interface ProfesseurServices {
-	List<Professeur> findAllProfesseurs();
-	Professeur findProfesseurbyid(Long id);
-	Professeur CreateProfesseur(Professeur a);
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
+public class ProfesseurServices {
+	private final ProfesseurRepository professeurRepository;
+	public List<Professeurdto> findAll(){
+		 return professeurRepository.findAll() // récupère tous les professeurs depuis la base de données
+		.stream() //transform list des proffesseur en flux
+		.map(Professeurdto::fromEntity) //transform chaque element en flux
+		.collect(Collectors.toList());		
+	}
+	public Professeurdto findProfById(Long id) {
+		Optional<Professeur> optionalprof =professeurRepository.findById(id);
+		optionalprof.orElseThrow(() -> new RuntimeException("prf not found with id : " +id ));
+		return Professeurdto.fromEntity(optionalprof.get());
+	}
+	public void deleteProfById(Long idprof) {
+		professeurRepository.deleteById(idprof);
+	}
 	
 }
